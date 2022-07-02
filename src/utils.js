9a914b417282eb73434e3e2b3a3dc06effbe4298
himanshuHeliverse <himanshu@heliverse.com>
2022-07-02 22:23:29 +0530

    first

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,6 @@ export const isLoggedIn = () => {
 const Token =localStorage.getItem("Token")
 console.log(Token)
 }
-exports.logout = () => { localStorage.clear(); window.location.reload() }
-exports.getUserId = () => localStorage.getItem('id')
-exports.getToken = () => localStorage.getItem('Token')
+// exports.logout = () => { localStorage.clear(); window.location.reload() }
+// exports.getUserId = () => localStorage.getItem('id')
+// exports.getToken = () => localStorage.getItem('Token')
